5309daa2162589476d513c849f4503b11b99c23c
Sandeep Nilla <sandeep.nillaibm.com@Sandeeps-MacBook-Air.local>
2017-07-28 15:23:06 -0500

    test for palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,2 +1,25 @@
-describe('the palindrome canary spec', () => {
+describe.only('the palindrome canary spec', () => {
+  it('Shows the test infrastructure works', () => {
+    true.should.be.true();
+  });
+
+
+  function isPalindrome(phrase) {
+    return phrase.split('').reverse().join('') === phrase;
+  }
+
+  describe(' a palindrome ', () => {
+    it('is "racecar"', () => {
+      isPalindrome('racecar').should.be.true();
+    });
+    it('is not "racecars"', () => {
+      isPalindrome('racecars').should.be.false();
+    });
+    it('is not a "race car"', () => {
+
+    });
+    it('"   " is not a palindrome');
+    it('"" is not a palindrome');
+    it('is "mom dad mom"');
+  });
 });
