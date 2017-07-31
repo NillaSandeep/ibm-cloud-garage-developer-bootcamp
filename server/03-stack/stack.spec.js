8c34871f4e39e7dfe16a5932bfeac2ce7c923da4
Sandeep Nilla <sandeep.nillaibm.com@sandeeps-air.dfw.ibm.com>
2017-07-31 09:31:56 -0500

    going back to scratch

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,34 +1,8 @@
-  const makeStack = () => {
-    let length = 0;
-    return {
-      isEmpty: () => length === 0,
-      push: () => length++,
-      size: () => length
-    };
-  };
-
-  let stack;
-
-  describe.only('the stack', () => {
-    beforeEach( () => {
-      stack = makeStack();
-    });
-
-  it('starts empty', () => {
-    stack.isEmpty().should.be.true();
-  });
-  it('starts with stack size 0', () => {
-    stack.size().should.equal(0);
-  });
-  it('is not be empty when pushed', () => {
-    stack.push();
-    stack.isEmpty().should.be.false();
-  });
-  it('leaves stack size 1 when pushed', () => {
-    stack.push();
-    console.log(stack.size());
-    stack.size().should.equal(1);
-  });
+describe.only('the stack spec', () => {
+  it('starts empty');
+  it('starts with stack size 0');
+  it('is not be empty when pushed');
+  it('leaves stack size 1 when pushed');
   it('leaves stack empty when pushed and popped');
   it('leaves stack size 0 when pushed and popped');
   it('overflows');
@@ -37,4 +11,3 @@
   it('pops the same two pushed');
   it('accepts only positive capacity');
 });
-
