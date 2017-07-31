dd6174fc1e9a9dd7137122176450ce99743dd6a1
Sandeep Nilla <sandeep.nillaibm.com@sandeeps-air.dfw.ibm.com>
2017-07-31 11:09:18 -0500

    changes made to test the push and pop stack

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,16 +1,44 @@
-let stack = {
-  isEmpty: () => true
+const makeStack = () => {
+  let stackSize = 0;
+  return {
+    isEmpty: () => stackSize === 0,
+    size: () => stackSize,
+    push: () => {
+      stackSize++;
+    },
+    pop: () => {
+      stackSize--;
+    }
+  };
 };
 
+let stack;
+
 describe.only('the stack spec', () => {
+  beforeEach(() => {
+    stack = makeStack();
+  });
   it('starts empty', () => {
     stack.isEmpty().should.be.true();
   });
 
-  it('starts with stack size 0');
-  it('is not be empty when pushed');
-  it('leaves stack size 1 when pushed');
-  it('leaves stack empty when pushed and popped');
+  it('starts with stack size 0', () => {
+    stack.size().should.equal(0);
+  });
+
+  it('is not be empty when pushed', () => {
+    stack.push();
+    stack.isEmpty().should.be.false();
+  });
+  it('leaves stack size 1 when pushed', () => {
+    stack.push();
+    stack.size().should.equal(1);
+  });
+  it('leaves stack empty when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    stack.isEmpty().should.be.true();
+  });
   it('leaves stack size 0 when pushed and popped');
   it('overflows');
   it('under-flows');
